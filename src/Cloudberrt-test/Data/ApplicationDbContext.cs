1948841274e5d84322bfe82d6fb8ecd4a9d45615
ashyapeachie <128183143+ashyapeachie@users.noreply.github.com>
2026-06-11 13:56:29 -0400

    finish code for ApplicationDbContext.cs

--- src/Cloudberrt-test/Data/ApplicationDbContext.cs
+++ src/Cloudberrt-test/Data/ApplicationDbContext.cs
@@ -3,7 +3,15 @@ using Cloudberry.Models;
 
 namespace Cloudberry.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MoodEntry> MoodEntires { get; set; }
+        public DbSet<JournalEntry> JournalEntries { get; set; }
+        public DbSet<CloudCompanion> CloudCompanions { get; set; }
     }
 }
