7ca54182e6634e4f4caea5b8e56e51d149882b75
ashyapeachie <128183143+ashyapeachie@users.noreply.github.com>
2026-06-10 23:13:27 -0400

    code for JournalEntry.cs and CloudCompanion.cs

--- src/Cloudberrt-test/Models/CloudCompanion.cs
+++ src/Cloudberrt-test/Models/CloudCompanion.cs
@@ -2,5 +2,9 @@
 {
     public class CloudCompanion
     {
+        public int Id {get; set;}
+        public string Name {get; set;} = "Cloudberry";
+        public int GrowthPoints {get; set;}
+        public string GrowthStage {get; set;} = "Tiny Cloud";
     }
 }
--- src/Cloudberrt-test/Models/JournalEntry.cs
+++ src/Cloudberrt-test/Models/JournalEntry.cs
@@ -2,5 +2,9 @@
 {
     public class JournalEntry
     {
+        public int Id {get; set;}
+        public string Title {get; set;} = string.Empty;
+        public string Content {get; set;} = string.Empty;
+        public DateTime DateCreated {get; set;} = DateTime.Now;
     }
 }
