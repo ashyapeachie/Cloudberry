20287fa866a3dd8100d55a63bb4734423d90490c
ashyapeachie <128183143+ashyapeachie@users.noreply.github.com>
2026-06-13 23:17:29 -0400

    finished code for MoodController, created & finished code for Create.cshtml

--- src/Cloudberry/Controllers/MoodController.cs
+++ src/Cloudberry/Controllers/MoodController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cloudberry.Data;
+using Cloudberry.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Cloudberrt_test.Controllers
+namespace Cloudberry.Controllers
 {
     public class MoodController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public MoodController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var moods = _context.MoodEntries
+                .OrderByDescending(m => m.DateCreated)
+                .ToList();
+
+            return View(moods);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MoodEntry moodEntry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.MoodEntries.Add(moodEntry);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(moodEntry);
         }
     }
 }
